47f89a3a07bfb415631aaac1914a3eb78ea80afb
Potato22 <potatooosss22@gmail.com>
2025-05-02 16:47:54 +0700

    milestone, success NOT reinventing the dragndrop wheel

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -98,90 +98,92 @@ export const cardList: Record<string, CardData> = {
         ],
         isDisabled: false,
         configData: [
-            {
-                subCategory: "chrDetail",
-                type: "singleChoice",
-                id: "character-detail",
-                options: [
-                    {
-                        optionName: "Sketch",
-                        optionDescription: `Loose and feels more "Freestyle" <br>Freestyle coloring can be requested, naturally adds a little extra fee. <span style="font-family: var(--contentSecondary)">(Est. price addition: 5 ish)</span>`,
-                        optionPrice: 25,
-                        basePrice: true,
-                        preChecked: true
-                    },
-                    {
-                        optionName: "Linart",
-                        optionDescription: "Cleaner lines",
-                        optionPrice: 30
-                    },
-                    {
-                        optionName: "Flat Color",
-                        optionDescription: "Cleaner lines & Light render",
-                        optionPrice: 45
-                    },
-                    {
-                        optionName: "Fully Rendered",
-                        optionDescription: "Best render and visual",
-                        optionPrice: 60
-                    },
-                ],
-                required: true,
-            },
-            {
-                subCategory: "bodyForm",
-                type: "flipflop",
-                id: "anthro",
-                options: [
-                    { optionName: "No", optionDescription: `"I like me some lil quadruped creatur"`, optionPrice: 0, preChecked: true },
-                    { optionName: "Yes", optionDescription: "Anthro is significantly more complex than squishy ponies.", optionPrice: 10 },
-                ],
-            },
-            {
-                subCategory: "background",
-                type: "singleChoice",
-                id: "background",
-                options: [
-                    {
-                        optionName: "None/Simple",
-                        optionDescription: "None at all (transparent), Solid, gradient, or even with quirky particles as backgrounds",
-                        optionPrice: 0
-                    },
-                    {
-                        optionName: "Basic",
-                        optionDescription: "Basic details, simplified rendering",
-                        optionPrice: 35
-                    },
-                    {
-                        optionName: "Complex",
-                        optionDescription: `Complex and well rendered background. <br>(Only available for <span class="b7">Fully Rendered</span> character detail)`,
-                        optionPrice: 50,
-                        visibleIf: { questionId: "character-detail", value: "Fully Rendered" }
-                    },
-                    {
-                        optionName: "This is a robbery, fuck you",
-                        optionDescription: `Fuck the wallet, I'm taking the whole bank`,
-                        optionPrice: 7236493,
-                        visibleIf: { questionId: "character-detail", value: "Fully Rendered" }
-                    },
-                ],
-                required: true,
-            },
-            {
-                subCategory: "nsfw",
-                type: "flipflop",
-                id: "nsfw",
-                options: [
-                    { optionName: "No", optionDescription: "Pure as a summer.", optionPrice: 0, preChecked: true },
-                    { optionName: "Yes", optionDescription: "NSFW may require extra effort.", optionPrice: 15 },
-                ],
-            },
+            //{
+            //    subCategory: "chrDetail",
+            //    type: "singleChoice",
+            //    id: "character-detail",
+            //    options: [
+            //        {
+            //            optionName: "Sketch",
+            //            optionDescription: `Loose and feels more "Freestyle" <br>Freestyle coloring can be requested, naturally adds a little extra fee. <span style="font-family: var(--contentSecondary)">(Est. price addition: 5 ish)</span>`,
+            //            optionPrice: 25,
+            //            basePrice: true,
+            //            preChecked: true
+            //        },
+            //        {
+            //            optionName: "Linart",
+            //            optionDescription: "Cleaner lines",
+            //            optionPrice: 30
+            //        },
+            //        {
+            //            optionName: "Flat Color",
+            //            optionDescription: "Cleaner lines & Light render",
+            //            optionPrice: 45
+            //        },
+            //        {
+            //            optionName: "Fully Rendered",
+            //            optionDescription: "Best render and visual",
+            //            optionPrice: 60
+            //        },
+            //    ],
+            //    required: true,
+            //},
+            //{
+            //    subCategory: "bodyForm",
+            //    type: "flipflop",
+            //    id: "anthro",
+            //    options: [
+            //        { optionName: "No", optionDescription: `"I like me some lil quadruped creatur"`, optionPrice: 0, preChecked: true },
+            //        { optionName: "Yes", optionDescription: "Anthro is significantly more complex than squishy ponies.", optionPrice: 10 },
+            //    ],
+            //},
+            //{
+            //    subCategory: "background",
+            //    type: "singleChoice",
+            //    id: "background",
+            //    options: [
+            //        {
+            //            optionName: "None/Simple",
+            //            optionDescription: "None at all (transparent), Solid, gradient, or even with quirky particles as backgrounds",
+            //            optionPrice: 0
+            //        },
+            //        {
+            //            optionName: "Basic",
+            //            optionDescription: "Basic details, simplified rendering",
+            //            optionPrice: 35
+            //        },
+            //        {
+            //            optionName: "Complex",
+            //            optionDescription: `Complex and well rendered background. <br>(Only available for <span class="b7">Fully Rendered</span> character detail)`,
+            //            optionPrice: 50,
+            //            visibleIf: { questionId: "character-detail", value: "Fully Rendered" }
+            //        },
+            //        {
+            //            optionName: "This is a robbery, fuck you",
+            //            optionDescription: `Fuck the wallet, I'm taking the whole bank`,
+            //            optionPrice: 7236493,
+            //            visibleIf: { questionId: "character-detail", value: "Fully Rendered" }
+            //        },
+            //    ],
+            //    required: true,
+            //},
+            //{
+            //    subCategory: "nsfw",
+            //    type: "flipflop",
+            //    id: "nsfw",
+            //    options: [
+            //        { optionName: "No", optionDescription: "Pure as a summer.", optionPrice: 0, preChecked: true },
+            //        { optionName: "Yes", optionDescription: "NSFW may require extra effort.", optionPrice: 15 },
+            //    ],
+            //},
             {
                 type: "fileUpload",
                 subCategory: "character",
                 questionTitle: "Upload your character",
                 questionDescription: "Upload your character reference (sheet or images) here!",
                 id: "character-reference",
+                required: true,
+                maxFiles: 5,
             },
             {
                 type: "textarea",
@@ -199,6 +201,7 @@ export const cardList: Record<string, CardData> = {
                 questionDescription: `Most active chats: <span class="b7">Discord</span>, <span class="b7">Twitter</span>. <br>If you have none of these, you could provide an <span class="b7">email address</span> instead.`,
                 id: "contacts",
                 placeholder: `@yourTag OR your@email.com`,
+                required: true,
             }
         ],
     },
