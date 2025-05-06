04b24a53693e17cf6f9d0490148d29ed6483a54f
Potato22 <potatooosss22@gmail.com>
2025-05-06 20:04:21 +0700

    it's really inevitable this would happen

--- src/data/cardData.ts
+++ src/data/cardData.ts
@@ -42,10 +42,10 @@ export const cardList: Record<string, CardData> = {
         title: "Sketch Book",
         description: `Compile a collection of little <span class="b7">sketches</span> of your character(s) into a single canvas.`,
         images: [
-            { src: "/imgs/showcase/sb1.jpg" },
-            { src: "/imgs/showcase/sb2.jpg" },
-            { src: "/imgs/showcase/sb3.jpg" },
-            { src: "/imgs/showcase/sb4.jpg" }
+            { src: "imgs/showcase/sb1.jpg" },
+            { src: "imgs/showcase/sb2.jpg" },
+            { src: "imgs/showcase/sb3.jpg" },
+            { src: "imgs/showcase/sb4.jpg" }
         ],
         price: 10,
         tags: [
@@ -61,10 +61,10 @@ export const cardList: Record<string, CardData> = {
         title: "Headshot",
         description: "Headshot features the character's face and bust.",
         images: [
-            { src: "/imgs/showcase/portrait1.jpg" },
-            { src: "/imgs/showcase/portrait2.jpg" },
-            { src: "/imgs/showcase/portrait3.jpg" },
-            { src: "/imgs/showcase/portrait4.jpg" }
+            { src: "imgs/showcase/portrait1.jpg" },
+            { src: "imgs/showcase/portrait2.jpg" },
+            { src: "imgs/showcase/portrait3.jpg" },
+            { src: "imgs/showcase/portrait4.jpg" }
         ],
         price: 15,
         tags: [
@@ -80,10 +80,10 @@ export const cardList: Record<string, CardData> = {
         title: "Fullbody",
         description: "Display the full body of your character(s) of choice.",
         images: [
-            { src: "/imgs/showcase/fullbod1.jpg" },
-            { src: "/imgs/showcase/fullbod2.jpg" },
-            { src: "/imgs/showcase/fullbod3.jpg" },
-            { src: "/imgs/showcase/fullbod4.jpg", pos: "top" },
+            { src: "imgs/showcase/fullbod1.jpg" },
+            { src: "imgs/showcase/fullbod2.jpg" },
+            { src: "imgs/showcase/fullbod3.jpg" },
+            { src: "imgs/showcase/fullbod4.jpg", pos: "top" },
         ],
         price: 25,
         tags: [
