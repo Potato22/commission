9e5bb92a2bbf5346a218d4c4cb5e96e0d28dbde1
Potato22 <potatooosss22@gmail.com>
2025-05-06 19:53:40 +0700

    try gh deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://potato22.github.io',
+    base: 'commission'
+});
