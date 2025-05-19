55fb4201ac16c6ecfaad75007ac7128b83a8284b
Potato22 <potatooosss22@gmail.com>
2025-05-20 02:03:30 +0700

    TOS page wip, mdx plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,27 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
     site: 'https://commission.pottoart.uk',
+
     //base: 'commission' //repo name
+    //does fucking nothing.
+    markdown: {
+        syntaxHighlight: "shiki",
+        shikiConfig: {
+            themes: {
+                light: 'github-light',
+                dark: 'github-dark',
+            },
+            wrap: true,
+            langAlias: {
+                cjs: "javascript"
+            }
+        }
+    },
+
+    integrations: [mdx()]
 });
